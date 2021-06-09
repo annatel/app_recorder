ee0516823467f4b6d17fdeae785a34ec6abf6cc3
Eliel Haouzi <eliel.haouzi@gmail.com>
2021-06-09 16:38:45 +0300

    add RecordRequest

--- lib/app_recorder/plug/record_request.ex
+++ lib/app_recorder/plug/record_request.ex
@@ -1,140 +1,236 @@
-# defmodule Plug.RecordRequest do
-#   @moduledoc """
-#   A plug to record the request. It supports an idempotency_key.
+defmodule AppRecorder.Plug.RecordRequest do
+  use Plug.ErrorHandler
+  require Logger
 
-#   If an idempotency key exists as the "idempotency_key" HTTP request header,
-#   then that value will be used assuming it is between 20 and 200 characters.
-#   If it is not, the idempotency key will be ignored.
+  alias Plug.Conn
 
-#   The idempotency key is added to the Logger metadata as `:idempotency_key` and the response as
-#   the "idempotency_key" HTTP header.
+  alias AppRecorder.Requests
+  alias AppRecorder.Requests.Request
 
-#   To use this plug, just plug it into the desired module:
+  @behaviour Plug
 
-#       plug Plug.RecordRequest
+  @idempotency_key_header "idempotency-key"
+  @idempotent_replayed_header "idempotent-replayed"
+  @original_request_id_header "original-request-id"
 
-#   ## Options
+  @spec init(keyword) :: keyword
+  def init(_), do: []
 
-#     * `:http_header` - The name of the HTTP *request* header to check for
-#       existing idempotency_key. This is also the HTTP *response* header that will be
-#       set with the idempotency_key. Default value is "idempotency-key"
+  @spec call(Conn.t(), keyword) :: Conn.t()
+  def call(%Plug.Conn{} = conn, _) do
+    idempotency_key = get_idempotency_key(conn, @idempotency_key_header)
 
-#           plug Plug.RecordRequest, http_header: "custom-idempotency-key"
+    conn
+    |> set_idempotency_key(idempotency_key)
+    |> put_original_request()
+    |> save_current_request_data()
+    |> maybe_resp_original_request()
+    |> Plug.Conn.register_before_send(fn conn ->
+      record_response_data!(conn, conn.assigns.current_request)
 
-#   """
+      conn
+    end)
+  end
 
-#   require Logger
+  def handle_errors(conn, %{kind: _kind, reason: _reason, stack: _stack}) do
+    Conn.send_resp(conn, conn.status, "Something went wrong")
+  end
 
-#   alias Plug.Conn
+  defp get_idempotency_key(conn, idempotency_key_header) do
+    case Conn.get_req_header(conn, idempotency_key_header) do
+      [val | _] -> if valid_idempotency_key?(val), do: val, else: nil
+      [] -> nil
+    end
+  end
 
-#   @behaviour Plug
+  defp set_idempotency_key(%Plug.Conn{} = conn, nil), do: conn
 
-#   @idempotency_key_header "idempotency-key"
-#   @idempotent_replayed_header "idempotent-replayed"
-#   @original_request_id_header "original-request-id"
+  defp set_idempotency_key(%Plug.Conn{} = conn, idempotency_key) do
+    Logger.metadata(idempotency_key: idempotency_key)
 
-#   def init(opts) do
-#     %{
-#       idempotency_key_header: Keyword.get(opts, :http_header, @idempotency_key_header),
-#       get_original_request_fun: Keyword.get(opts, :get_original_request),
-#       save_request_data_fun: Keyword.get(opts, :save_request_data),
-#       save_response_data_fun: Keyword.get(opts, :save_response_data),
-#       response_content_type_fun: Keyword.get(opts, :response_content_type),
-#       response_raw_body_fun: Keyword.get(opts, :response_raw_body),
-#       response_status_fun: Keyword.get(opts, :response_status)
-#     }
-#   end
+    conn
+    |> Conn.put_private(:idempotency_key, idempotency_key)
+    |> Conn.put_resp_header(@idempotency_key_header, idempotency_key)
+  end
 
-#   def call(%Plug.Conn{} = conn, %{idempotency_key_header: idempotency_key_header} = opts) do
-#     opts = Map.put(opts, :idempotency_key, get_idempotency_key(conn, idempotency_key_header))
+  defp put_original_request(
+         %Plug.Conn{method: "POST", private: %{idempotency_key: idempotency_key}} = conn
+       )
+       when is_binary(idempotency_key) do
+    conn
+    |> Conn.put_private(
+      :original_request,
+      AppRecorder.Requests.get_request_by(idempotency_key: idempotency_key)
+    )
+    |> Conn.put_private(:replayed_request?, not is_nil(conn.private.original_request))
+  end
 
-#     conn
-#     |> set_idempotency_key(opts)
-#     |> put_original_request(opts)
-#     |> save_current_request_data(opts)
-#     |> maybe_resp_original_request()
-#     |> Plug.Conn.register_before_send(fn conn ->
-#       save_response_data.(conn, conn.assigns.current_request)
+  defp put_original_request(%Plug.Conn{} = conn),
+    do: conn |> Conn.put_private(:replayed_request?, false)
 
-#       conn
-#     end)
-#   end
+  defp save_current_request_data(conn) do
+    original_request = conn.private[:original_request]
+    current_request = record_request!(conn, original_request)
 
-#   defp get_idempotency_key(conn, http_header) do
-#     case Conn.get_req_header(conn, http_header) do
-#       [val | _] -> if valid_idempotency_key?(val), do: val, else: nil
-#       [] -> nil
-#     end
-#   end
+    conn
+    |> Conn.put_private(:current_request, current_request)
+  end
 
-#   defp set_idempotency_key(%Plug.Conn{} = conn, %{idempotency_key: nil}), do: conn
+  defp maybe_resp_original_request(conn) do
+    original_request = conn.private[:original_request]
 
-#   defp set_idempotency_key(%Plug.Conn{} = conn, %{
-#          idempotency_key_header: idempotent_key_header,
-#          idempotency_key: idempotency_key
-#        }) do
-#     Logger.metadata(idempotency_key: idempotency_key)
+    if conn.private.replayed_request? do
+      conn
+      |> Conn.put_resp_header(@idempotent_replayed_header, true)
+      |> Conn.put_resp_header(@original_request_id_header, original_request.id)
+      |> Conn.put_resp_content_type(response_content_type(original_request))
+      |> Conn.send_resp(response_status(original_request), response_body(original_request))
+      |> Conn.halt()
+    else
+      conn
+    end
+  end
 
-#     conn
-#     |> Conn.put_resp_header(http_header, idempotency_key)
-#   end
+  defp valid_idempotency_key?(s), do: byte_size(s) in 20..200
 
-#   defp put_original_request(%Plug.Conn{method: "POST"} = conn, %{
-#          idempotency_key: idempotency_key,
-#          get_original_request_fun: get_original_request_fun
-#        })
-#        when not is_nil(idempotency_key) do
-#     conn
-#     |> Conn.put_private(:original_request, get_original_request_fun.(idempotency_key))
-#   end
+  defp record_request!(%Plug.Conn{} = conn, original_request) do
+    conn
+    |> build_request_attrs(original_request)
+    |> AppRecorder.Requests.record_request!()
+  end
 
-#   defp put_original_request(%Plug.Conn{} = conn, _), do: conn
+  defp record_response_data!(%Plug.Conn{} = conn, request) do
+    request
+    |> Requests.update_request!(%{
+      success: conn.status in 200..299,
+      response_data: %{
+        body: response_body(conn),
+        headers: response_headers(conn),
+        status: conn.status
+      }
+    })
+  end
 
-#   defp save_current_request_data(conn, %{save_request_data_fun: save_request_data_fun}) do
-#     original_request = Map.get(conn.assigns, :original_request)
-#     current_request = save_request_data_fun.(conn, original_request)
+  defp build_request_attrs(%Plug.Conn{} = conn, nil) do
+    owner_id_field_name = elem(AppRecorder.owner_id_field(), 0)
 
-#     conn
-#     |> Conn.put_private(:current_request, current_request)
-#   end
+    attrs =
+      %{
+        id: Logger.metadata()[:request_id],
+        created_at: DateTime.utc_now(),
+        idempotency_key: conn.private.idempotency_key,
+        request_data: %{
+          body: request_body(conn),
+          client_ip: Logger.metadata()[:remote_ip],
+          headers: request_headers(conn),
+          method: conn.method,
+          path: conn.request_path,
+          query_params: conn.query_params,
+          query_string: conn.query_string,
+          url: url(conn)
+        }
+      }
+      |> Map.put(owner_id_field_name, Map.get(conn.assigns, owner_id_field_name))
 
-#   defp maybe_resp_original_request(
-#          conn,
-#          %{
-#            response_content_type_fun: response_content_type_fun,
-#            response_raw_body_fun: response_raw_body_fun,
-#            response_status_fun: response_status_fun
-#          }
-#        ) do
-#     original_request = Map.get(conn.assigns, :original_request)
+    if AppRecorder.with_livemode?(),
+      do: Map.put(attrs, :livemode, conn.assigns["livemode?"]),
+      else: attrs
+  end
 
-#     if original_request do
-#       conn
-#       |> Conn.put_resp_header(@idempotent_replayed_header, true)
-#       |> Conn.put_resp_header(@original_request_id_header, original_request.id)
-#       |> put_resp_content_type(response_content_type_fun(original_request))
-#       |> send_resp(response_status_fun(original_request), response_raw_body_fun(original_request))
-#       |> halt()
-#     else
-#       conn
-#     end
-#   end
+  defp build_request_attrs(%Plug.Conn{} = conn, %Request{} = request) do
+    conn
+    |> build_request_attrs(nil)
+    |> Map.merge(%{
+      response_data: %{
+        body: response_body(request),
+        headers: response_headers(conn),
+        status: response_status(request)
+      },
+      source: request.source,
+      success: request.success
+    })
+  end
 
-#   defp valid_idempotency_key?(s), do: byte_size(s) in 20..200
+  defp request_body(%Conn{} = conn) do
+    request_content_type = request_content_type(conn)
 
-#   defp get_response_body(%Plug.Conn{} = conn) do
-#     conn
-#     |> get_response_content_type()
-#     |> case do
-#       "application/json; charset=utf-8" -> Phoenix.json_library().decode!(conn.resp_body)
-#       _ -> conn.resp_body
-#     end
-#   end
+    cond do
+      request_content_type =~ "application/json" ->
+        Jason.encode!(conn.body_params)
 
-#   defp get_response_content_type(%Plug.Conn{} = conn) do
-#     case List.keyfind(conn.resp_headers, "content-type", 0) do
-#       {_, content_type} -> content_type
-#       _ -> conn.resp_body
-#     end
-#   end
-# end
+      request_content_type =~ "application/x-www-form-urlencoded" ->
+        Jason.encode!(conn.body_params)
+
+      true ->
+        ""
+    end
+  end
+
+  defp request_content_type(%Conn{} = conn) do
+    case List.keyfind(conn.req_headers, "content-type", 0) do
+      {_, content_type} -> content_type
+      nil -> nil
+    end
+  end
+
+  defp request_headers(%Conn{} = conn) do
+    ["content-type", "origin", "referer", "user-agent", "x-tls-version"]
+    |> Enum.reduce(%{}, fn header, acc ->
+      {_, value} = List.keyfind(conn.req_headers, header, 0)
+
+      Map.put(acc, header, value)
+    end)
+    |> Enum.reject(&is_nil(&1))
+  end
+
+  defp response_body(%Conn{} = conn) do
+    response_content_type = response_content_type(conn)
+
+    cond do
+      response_content_type =~ "application/json" ->
+        Jason.encode!(conn.resp_body)
+
+      true ->
+        conn.resp_body
+    end
+  end
+
+  defp response_body(%Request{response_data: %{body: body}}), do: body
+
+  defp response_content_type(%Conn{} = conn) do
+    case List.keyfind(conn.resp_headers, "content-type", 0) do
+      {_, content_type} -> content_type
+      nil -> nil
+    end
+  end
+
+  defp response_content_type(%Request{response_data: %{headers: headers}}),
+    do: Map.get(headers, "content-type")
+
+  defp response_headers(%Conn{} = conn) do
+    [
+      "content-length",
+      "content-type",
+      "idempotency-key",
+      @idempotent_replayed_header,
+      @original_request_id_header,
+      "request-id"
+    ]
+    |> Enum.reduce(%{}, fn header, acc ->
+      {_, value} = List.keyfind(conn.resp_headers, header, 0)
+
+      Map.put(acc, header, value)
+    end)
+    |> Enum.reject(&is_nil(&1))
+  end
+
+  defp response_status(%Request{response_data: %{status: status}}), do: status
+
+  defp url(%Conn{port: port} = conn) when port in [80, 443] do
+    "#{conn.scheme}://#{conn.host}#{conn.request_path}"
+  end
+
+  defp url(%Conn{} = conn) do
+    "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}"
+  end
+end
